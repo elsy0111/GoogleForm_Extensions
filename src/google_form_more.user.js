71dbe2b0e56eaa95cd9d67c10c2ab6e410ba59e2
elsy <kpp0193@gmail.com>
2024-07-12 00:03:30 +0900

    add clear src

---
+++ src/google_form_more.user.js
@@ -0,0 +1,78 @@
+// ==UserScript==
+// @name         Google Forms More
+// @namespace    https://github.com/elsy0111/GoogleForm_Extensions
+// @version      1.0
+// @description  Google Forms More
+// @author       Arcsecond
+// @match        ://docs.google.com/forms/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.onload = function() {
+        setTimeout(function() {
+
+            var checkboxes = document.querySelectorAll('div[role="checkbox"]');
+
+            checkboxes.forEach(function(checkbox) {
+                while (checkbox.firstChild) {
+                    checkbox.removeChild(checkbox.firstChild);
+                }
+                checkbox.style.borderColor = '#EEE';
+            });
+
+            var textInputs = document.querySelectorAll('input[type="text"]');
+
+            textInputs.forEach(function(input) {
+                input.style.color = '#FFF';
+            });
+            var visibleElements = document.querySelectorAll('[style="visibility: visible;"]');
+
+            visibleElements.forEach(function(element) {
+                element.style.visibility = 'hidden';
+            });
+
+            var elements = document.querySelectorAll('[aria-describedby][role="group"]');
+            elements.forEach(function(element) {
+                var childElements = element.querySelectorAll('*');
+                childElements.forEach(function(childElement) {
+                    childElement.style.backgroundColor = 'rgb(255, 255, 255)';
+                    childElement.style.color = '#222';
+                });
+            });
+            var ans_check = document.querySelectorAll('[aria-label]');
+            ans_check.forEach(function(ans) {
+                while (ans.firstChild) {
+                    ans.removeChild(ans.firstChild);
+                }
+            });
+            var ques = document.querySelectorAll('[role="heading"]');
+            ques.forEach(function(q) {
+                q.style.color = 'rgb(32,33,36)';
+            });
+
+
+
+            /*var ariaexpCheckedElements = document.querySelectorAll('[aria-expanded]');
+
+            ariaexpCheckedElements.forEach(function(checkbox) {
+                while (checkbox.firstChild) {
+                    checkbox.removeChild(checkbox.firstChild);
+                }
+                checkbox.style.width = '50%';
+                checkbox.style.height = '40px';
+                checkbox.style.color = '#EEE';
+            });*/
+            var ariaCheckedElements = document.querySelectorAll('[aria-checked]');
+
+            ariaCheckedElements.forEach(function(checkbox) {
+                while (checkbox.firstChild) {
+                    checkbox.removeChild(checkbox.firstChild);
+                }
+                checkbox.style.backgroundColor = '#EEE';
+            });
+        }, 1000);
+    };
+})();
